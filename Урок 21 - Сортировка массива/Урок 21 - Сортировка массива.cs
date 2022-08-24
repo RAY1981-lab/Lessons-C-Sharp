33af51f6a90277fa88aa2ed7f3647d5200039711
Юрченко Роман <ray1981@yandex.ru>
2022-08-24 17:20:29 +0300

    Конец рабоченго дня 24.08.2022

--- Урок 21 - Сортировка массива/Урок 21 - Сортировка массива.cs
+++ Урок 21 - Сортировка массива/Урок 21 - Сортировка массива.cs
@@ -1,15 +1,69 @@
-﻿int[] CreateArray(int size)
+﻿int[] CreateArray(int size)  // Метод заполнения массива случайными целыми числами от -100 до 100
 {
     int[] array = new int[size];
     Random random = new Random();
     for (int i = 0; i < array.Length; i++)
-    {
         array[i] = random.Next(-100, 101);
-    }
-    
     return array;
 }
 
-Console.Write("Программа сортировки массива.\r\nЭлементы присваиваются автоматически в диапазоне от -100 до 100.\r\n Введите количество элементов в массиве.  ");
+int[] BubbleSortArray(int[] array)  // Сортировка массива пузырьковым методом
+{
+    for (int i = 0; i < array.Length; i++)
+        for (int j = 0; j < array.Length - 1; j++)
+            if (array[j] > array[j + 1])
+            {
+                int t = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = t;
+            }
+    return array;
+}
+
+
+int[] MergeSortArray(int[] array)  // Сортировка массива методом слияния
+{
+    for (int i = 0; i < array.Length; i++)
+        for (int j = 0; j < array.Length - 1; j++)
+            if (array[j] > array[j + 1])
+            {
+                int t = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = t;
+            }
+    return array;
+}
+
+
+
+
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("Программа сортировки массива.\r\nЭлементы присваиваются автоматически в диапазоне от -100 до 100.\r\nВведите количество элементов в массиве: ");
 int sizeArray = Convert.ToInt32(Console.ReadLine());
 int[] massive = CreateArray(sizeArray);
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Начальный вариант массива:");
+for (int i = 0; i < massive.Length; i++)
+    Console.Write(massive[i] + " ");
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Сортировка массива пузырьковым методом:");
+int[] bubble = BubbleSortArray(massive);
+for (int i = 0; i < massive.Length; i++)
+    Console.Write(bubble[i] + " ");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Сортировка массива пузырьковым методом:");
+int[] merge = MergeSortArray(massive);
+for (int i = 0; i < massive.Length; i++)
+    Console.Write(bubble[i] + " ");
+
+
+
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.White;
---
+++ Урок 22 - Нахождение координат пересекающихся прямых/Program.cs
@@ -0,0 +1,61 @@
+﻿double[] DataEntry()  // Метод ввода коэфициентов для уравнений прямых
+{ 
+    double[] dataEntry = new double[4];  // Массив из 4-х элементов: [0]-k1, [1]-k2, [2]-b1, [3]-b2
+    
+    Console.Write("Введите k1: ");
+    dataEntry[0] = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите b1: ");
+    dataEntry[2] = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите k2: ");
+    dataEntry[1] = Convert.ToInt32(Console.ReadLine());
+        
+    Console.Write("Введите b2: ");
+    dataEntry[3] = Convert.ToInt32(Console.ReadLine());
+
+    return dataEntry;
+}
+
+
+double[] IntersectionPoint (double[] dataEntryMassive)
+{
+    double[] dataEntryMassiveWork = dataEntryMassive;
+    double[] coordinateDotA = new double[2];  // Массив из 2-х элементов - координаты точки пересечения (при наличии) прямых на плоскости
+                                              // в виде A[0] - по оси Х, A[1] - по оси Y
+
+    do
+    {
+        if ((dataEntryMassiveWork[0] / dataEntryMassiveWork[1]) != (dataEntryMassiveWork[2] / dataEntryMassiveWork[3]))
+        {
+            Console.WriteLine("Прямые параллельны => не пересекаются!");
+            break;
+        }
+        else if ((dataEntryMassiveWork[0] / dataEntryMassiveWork[1]) == (dataEntryMassiveWork[2] / dataEntryMassiveWork[3]))
+        {
+            Console.WriteLine("Прямые совпадают друг с другом!");
+            break;
+        }
+        else if ((dataEntryMassiveWork[0] / dataEntryMassiveWork[1]) == 1)  // у Y в уравнениях нет коэффициентов => 1/1 = 1
+        {
+            coordinateDotA[0] = (dataEntryMassiveWork[3] - dataEntryMassiveWork[2]) /
+                                (dataEntryMassiveWork[0] - dataEntryMassiveWork[1]);
+
+            coordinateDotA[1] = (dataEntryMassiveWork[1] * dataEntryMassiveWork[2]) - (dataEntryMassiveWork[0] - dataEntryMassiveWork[3]) /
+                                                            ((dataEntryMassiveWork[1] - dataEntryMassiveWork[0]));
+            return coordinateDotA;
+        }
+    }
+    while (false);
+}
+
+
+Console.WriteLine("Программа - определятор координаты точки пересечения (при наличии) прямых на плоскости \r\n" +
+                  "Уравнения прямых выглядят следующим образом:\r\n" +
+                  "\ty=k1*x + b1\r\n" +
+                  "\ty=k2*x + b2");
+
+double[] dataEntry = DataEntry();
+Console.WriteLine($"Получились уравнения прямых следующего вида: y = {dataEntry[0]}x + {dataEntry[2]}  и  y = {dataEntry[1]}x + {dataEntry[3]}");
+
+
