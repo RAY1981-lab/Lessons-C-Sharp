54b382d7aa6dbbfad2c5506b1f86800119b34b37
RAY1981-lab <ray1981@yandex.ru>
2022-08-29 22:35:30 +0300

    вечер

---
+++ Урок 25 - ООП. Введение/Урок 25 - ООП. Введение.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    // ООП
+    // Класс
+    // Объект класса
+
+    enum Color
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        White,
+        Black,
+        Orange,
+        Violet
+    }
+
+    class Point
+    {
+        public int x;
+        public int y;
+        public Color color;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point pointDod1 = new Point();
+            pointDod1.x = 5;
+            pointDod1.y = 5;
+            pointDod1.color = Color.Green;
+            
+            Point pointDod2 = new Point();
+            pointDod2.x = 3;
+            pointDod2.y = 8;
+            pointDod2.color = Color.White;
+
+            Console.WriteLine($"У точки {pointDod1.color} координата ({pointDod1.x}, {pointDod1.y})");
+            Console.WriteLine($"У точки {pointDod2.color} координата ({pointDod2.x}, {pointDod2.y})");
+
+        }
+    }
+}
---
+++ Урок 26 - ООП. Пример со студентами/Урок 26 - ООП. Пример со студентами.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// ООП
+// Класс
+// Объект класса
+
+
+namespace LessonsStudent
+{
+    class Student
+    {
+        public Gu;
+        public int y;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point pointDod1 = new Point();
+            pointDod1.x = 5;
+            pointDod1.y = 5;
+            pointDod1.color = Color.Green;
+
+            Point pointDod2 = new Point();
+            pointDod2.x = 3;
+            pointDod2.y = 8;
+            pointDod2.color = Color.White;
+
+            Console.WriteLine($"У точки {pointDod1.color} координата ({pointDod1.x}, {pointDod1.y})");
+            Console.WriteLine($"У точки {pointDod2.color} координата ({pointDod2.x}, {pointDod2.y})");
+
+        }
+    }
+}
